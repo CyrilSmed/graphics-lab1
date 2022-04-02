6db2908fd1276a63a79a9f5801f0429e483a28f6
CyrilSmed <cyrilsmed@outlook.com>
2022-04-02 15:22:40 +0500

    Display three points

--- main.cpp
+++ main.cpp
@@ -1,26 +1,56 @@
-#include <GL/freeglut.h>
 #include <GL/glew.h>
-//#include "GL/math_3d.h"
+#include <GL/freeglut.h>
 #include <iostream>
+#include <glm/glm.hpp>
 
-static void RenderSceneCB()
+GLuint VBO;
+
+static void renderSceneCB()
 {
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+    glDrawArrays(GL_POINTS, 0, 3);
+
+    glDisableVertexAttribArray(0);
+
     glutSwapBuffers();
 }
+
+
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
     glutInitWindowSize(1024, 768);
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("Tutorial 01");
+    glutCreateWindow("Lab 1");
 
-    glutDisplayFunc(RenderSceneCB);
-    glClearColor(0.95f, 0.97f, 1.0f, 0.0f);
+    glutDisplayFunc(renderSceneCB);
+
+    // Must be done after glut is initialized!
+    GLenum res = glewInit();
+    if (res != GLEW_OK) {
+        fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+        return 1;
+    }
+
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+    glm::vec3 vecs[3] = {
+        {0.8f, 0.8f, 0.0f},
+        {0.0f, 0.0f, 0.0f},
+        {-0.8f, -0.2f, 0.0f}
+    };
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vecs), vecs, GL_STATIC_DRAW);
+
     glutMainLoop();
 
-    glClear(GL_COLOR_BUFFER_BIT);
-    glutSwapBuffers();
     return 0;
 }
